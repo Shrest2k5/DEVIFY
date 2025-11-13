40959562000e2d44519d030e7a0d0bd58a3de785
Shrest2k5 <shrestshaw2k5@gmail.com>
2025-11-13 23:59:09 +0530

    session routes & controllers added

--- Backend/source/server.js
+++ Backend/source/server.js
@@ -7,6 +7,7 @@ import {serve} from "inngest/express"
 import {clerkMiddleware} from '@clerk/express'
 import { inngest,functions } from "./lib/inngest.js";
 import chatRoutes from "./routes/chatRoutes.js"
+import sessionRoutes from "./routes/sessionRoutes.js"
 
 const app = express();
 
@@ -19,7 +20,8 @@ app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 app.use(clerkMiddleware());
 
 app.use("/api/inngest",serve({client:inngest,functions}));
-app.use("/api/chat", chatRoutes)
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 app.get("/health",(req,res)=>{
   res.status(200).json({msg:"api is up and running"})

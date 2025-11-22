3df87083a2a5a791dc004c76ab5915e209f7f4b0
Shrest2k5 <shrestshaw2k5@gmail.com>
2025-11-23 00:35:41 +0530

    session page added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
 
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser } from '@clerk/clerk-react'
+import {  useUser } from '@clerk/clerk-react'
 import { Navigate, Route,Routes } from 'react-router'
 import HomePage from './pages/HomePage';
-
 import ProblemsPage from "./pages/ProblemsPage"
 import ProblemPage from './pages/ProblemPage';
 import { Toaster } from 'react-hot-toast';
 import DashboardPage from './pages/DashboardPage';
+import SessionPage from './pages/SessionPage';
 
 function App() {
   
@@ -23,6 +23,7 @@ function App() {
 
       <Route path="/problems" element={isSignedIn ?<ProblemsPage/> : <Navigate to={"/"}/>}/>
       <Route path="/problem/:id" element={isSignedIn ?<ProblemPage/> : <Navigate to={"/"}/>}/>
+      <Route path="/session/:id" element={isSignedIn ?<SessionPage/> : <Navigate to={"/"}/>}/>
       
       </Routes>
       <Toaster  toastOptions={{duration:3000}}/>
